d1447d5f2dd4c847bcb2bf5d6ee9ab7fa5bd5250
Andrea Parodi <andrea@parro.it>
2016-10-15 11:21:19 +0200

    tweaks

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-import 'babel-register';
 import test from 'ava';
-import readlineCompleter from './main';
+import completer from './main';
 
 test('exports a function', t => {
-	t.is(typeof readlineCompleter, 'function');
+	t.is(typeof completer, 'function');
 });
